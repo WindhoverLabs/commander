f45010fe3b1bae5da47fead45e617fa0dbad03ff
Mathew Benson <mbenson@windhoverlabs.com>
2018-07-25 15:52:09 +0000

    Added test for for the new windowed layout.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testpage1Router = require('./routes/testpage1');
+var testpage2Router = require('./routes/testpage2');
 
 var app = express();
 
@@ -22,6 +24,8 @@ app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testpage1', testpage1Router);
+app.use('/testpage2', testpage2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
